114f90d997f506df2751d2debb0c89d96b56a139
tbaudoin <baudoin.thierry@gmail.com>
2022-11-24 12:23:56 +0100

    ajouts des solutions

--- syllabus_2/chapitres/diviseurs-et-multiples/helper.py
+++ syllabus_2/chapitres/diviseurs-et-multiples/helper.py
@@ -1,18 +1,6 @@
-from sympy.ntheory import factorint
+from numpy import gcd, lcm
 from pyperclip import copy
-from sympy import gcd, lcm
 
-exos=[(40, 16),
-(75, 90),
-(90, 48),
-(80, 84),
-(44, 55),
-(150, 300),
-(2, 3),
-(12, 15),
-(50, 75),
-(16, 12),
-(14, 10),
-(60, 35)]
-for t in exos:
-    print(lcm(t))
+
+# print(gcd.reduce([180, 240, 168]))
+print(lcm(18,15))
